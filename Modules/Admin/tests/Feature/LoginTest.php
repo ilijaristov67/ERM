330a30da62687a43c663b7f5d54e788b8102042c
ilijaristov67 <ilijaristov67@gmail.com>
2025-11-09 21:06:33 +0100

    finished tests for login

--- Modules/Admin/tests/Feature/LoginTest.php
+++ Modules/Admin/tests/Feature/LoginTest.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Admin\Tests\Feature\Authentications;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Modules\Admin\Models\Permission\Permission;
 use Modules\Admin\Models\User\User;
@@ -33,5 +34,54 @@ it('logins user', function () {
         'password' => 'password',
     ]);
 
-    dd($response->json());
+    expect($response->status())->toBe(200)
+        ->and($response->json())
+        ->toHaveKeys(['access_token', 'expires_at', 'user'])
+        ->and($response->json('expires_at'))->toBeString()->and(fn ($date) => expect(Carbon::parse($date))->toBeInstanceOf(Carbon::class))
+        ->and($response->json('user'))->toHaveKeys(['id', 'first_name', 'last_name', 'username', 'phone_number',
+            'roles', 'direct_permissions'])
+        ->and($response->json('user')['roles'])->toBeArray()->and($response->json('user')['roles'][0])
+        ->toHaveKeys(['id', 'name', 'permissions'])
+        ->and($response->json('user')['roles'][0]['permissions'])->toBeArray()
+        ->and($response->json('user')['roles'][0]['permissions'][0]['name'])->toBe('role')
+        ->and($response->json('user')['direct_permissions'])->toBeArray()
+        ->and($response->json('user')['direct_permissions'][0])->toHaveKeys(['id', 'name'])
+        ->and($response->json('user')['direct_permissions'][0]['name'])->toBe('direct');
+});
+
+it('successfully logs in with valid credentials and super admin gets all permissions', function () {
+    $this->user->removeRole($this->userRole);
+    $this->user->permissions()->sync([$this->permission1->id]);
+    $this->user->assignRole($this->superAdminRole);
+
+    $response = $this->postJson(route($this->routeName), [
+        'username' => 'user',
+        'password' => 'password',
+    ]);
+
+    expect($response->status())->toBe(200)
+        ->and($response->json())
+        ->toHaveKeys(['access_token', 'expires_at', 'user'])
+        ->and($response->json('expires_at'))->toBeString()->and(fn ($date) => expect(Carbon::parse($date))->toBeInstanceOf(Carbon::class))
+        ->and($response->json('user'))->toHaveKeys(['id', 'first_name', 'last_name', 'username', 'phone_number',
+            'roles', 'direct_permissions'])
+        ->and($response->json('user')['roles'])->toBeArray()->and($response->json('user')['roles'][0])
+        ->toHaveKeys(['id', 'name', 'permissions'])
+        ->and($response->json('user')['roles'][0]['permissions'])->toBeArray()->toHaveCount(3)
+        ->and($response->json('user')['roles'][0]['permissions'][0]['name'])->toBe('direct')
+        ->and($response->json('user')['roles'][0]['permissions'][1]['name'])->toBe('role')
+        ->and($response->json('user')['roles'][0]['permissions'][2]['name'])->toBe('super admin direct')
+        ->and($response->json('user')['direct_permissions'])->toBeArray()
+        ->and($response->json('user')['direct_permissions'][0]['name'])->toBe('super admin direct');
+});
+
+it('fails to log in with invalid credentials', function () {
+    $response = $this->postJson(route($this->routeName), [
+        'username' => 'use12r',
+        'password' => 'password1',
+    ]);
+
+    expect($response->status())
+        ->toBe(400)
+        ->and($response->json('message'))->toBe(__('Wrong credentials.'));
 });
