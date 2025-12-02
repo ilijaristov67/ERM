8a3b08d32d5b922ff6abec731d2c6aca6ae1a24e
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-02 14:41:23 +0100

    actions made

---
+++ Modules/Admin/app/Actions/Country/StoreCountry.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Admin\Actions\Country;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class StoreCountry
+{
+    use AsAction;
+
+    public function handle()
+    {
+        //`
+    }
+}
---
+++ Modules/Admin/app/Http/Requests/Country/StoreCountryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Admin\Http\Requests\Country;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCountryRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'unique:countries,name',
+            ],
+            'iso_alpha_2' => [
+                'required',
+                'string',
+                'unique:countries,iso_alpha_2',
+                'max:2'
+            ],
+            'iso_alpha_3' => [
+                'required',
+                'string',
+                'unique:countries,iso_alpha_3',
+                'max:3'
+            ],
+            'iso_numeric' => [
+                'required',
+                'string',
+                'unique:countries,iso_numeric',
+            ]
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('admin-countries-create');
+    }
+}
