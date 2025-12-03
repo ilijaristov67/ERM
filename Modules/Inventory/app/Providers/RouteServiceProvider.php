18124e65154b64adbfebbed99e815000bb931c52
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-03 18:44:56 +0100

    added Inventory Module

---
+++ Modules/Inventory/app/Providers/RouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Inventory\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $name = 'Inventory';
+
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        Route::middleware('api')->prefix('api/inventory')->name('api.inventory.')->group(module_path($this->name, '/routes/api.php'));
+    }
+}
