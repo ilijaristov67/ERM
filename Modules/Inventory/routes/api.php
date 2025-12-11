b4dafc8447e794a7765025ab38ec255a652af62e
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-11 23:51:16 +0100

    added route

--- Modules/Inventory/routes/api.php
+++ Modules/Inventory/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Inventory\Http\Controllers\Product\DeleteProductController;
+use Modules\Inventory\Http\Controllers\Product\IndexProductController;
 use Modules\Inventory\Http\Controllers\Product\PatchProductController;
 use Modules\Inventory\Http\Controllers\Product\StoreProductController;
 
@@ -11,4 +12,5 @@ Route::prefix('/products')
         Route::post('/', StoreProductController::class)->name('store')->middleware(['permission:inventory-products-create']);
         Route::delete('/{product}', DeleteProductController::class)->name('delete')->middleware(['permission:inventory-products-delete']);
         Route::patch('/{product}', PatchProductController::class)->name('patch')->middleware(['permission:inventory-products-update']);
+        Route::get('/', IndexProductController::class)->name('index')->middleware(['permission:inventory-products-read']);
     });
