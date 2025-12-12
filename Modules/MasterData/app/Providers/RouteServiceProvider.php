8c29729a26a93976c28dfab38b5e4aada9095b55
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-13 00:22:02 +0100

    added MasterData Module

--- Modules/MasterData/app/Providers/RouteServiceProvider.php
+++ Modules/MasterData/app/Providers/RouteServiceProvider.php
@@ -4,47 +4,33 @@ namespace Modules\MasterData\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 class RouteServiceProvider extends ServiceProvider
 {
     protected string $name = 'MasterData';
 
-    /**
-     * Called before routes are registered.
-     *
-     * Register any model bindings or pattern based filters.
-     */
     public function boot(): void
     {
         parent::boot();
+
+        $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
+        $this->app['router']->aliasMiddleware('permission', PermissionMiddleware::class);
+        $this->app['router']->aliasMiddleware('role_or_permission', RoleOrPermissionMiddleware::class);
     }
 
-    /**
-     * Define the routes for the application.
-     */
     public function map(): void
     {
         $this->mapApiRoutes();
-        $this->mapWebRoutes();
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     */
-    protected function mapWebRoutes(): void
-    {
-        Route::middleware('web')->group(module_path($this->name, '/Routes/web.php'));
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     */
     protected function mapApiRoutes(): void
     {
-        Route::middleware('api')->prefix('api')->name('api.')->group(module_path($this->name, '/routes/api.php'));
+        Route::middleware('api')
+            ->prefix('api/master-data')
+            ->name('api.master-data.')
+            ->group(module_path($this->name, '/routes/api.php'));
     }
 }
