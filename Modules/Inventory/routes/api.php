58f970813f1894cca98f339c7093d3e49db32322
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-13 20:52:52 +0100

    deleted products together

--- Modules/Inventory/routes/api.php
+++ Modules/Inventory/routes/api.php
@@ -1,13 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Inventory\Http\Controllers\Product\DeleteProductController;
-use Modules\Inventory\Http\Controllers\Product\IndexProductController;
-use Modules\Inventory\Http\Controllers\Product\PatchProductController;
-use Modules\Inventory\Http\Controllers\Product\StoreProductController;
-
-Route::prefix('/products')
-    ->name('products.')
-    ->group(function () {
-        Route::post('/', StoreProductController::class)->name('store')->middleware(['permission:inventory-products-create']);
-    });
