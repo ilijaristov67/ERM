93937a687db53c102e1ac899d3c0eab9e329545c
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-16 22:44:22 +0100

    added controller

---
+++ Modules/MasterData/app/Http/Controllers/Item/StoreItemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\MasterData\Http\Controllers\Item;
+
+use App\Http\Controllers\Controller;
+use Modules\MasterData\Actions\Item\StoreItem;
+use Modules\MasterData\Http\Requests\Item\StoreItemRequest;
+use Modules\MasterData\Http\Resources\Item\ItemResource;
+
+class StoreItemController extends Controller
+{
+    public function __invoke(StoreItemRequest $request):  ItemResource
+    {
+     return StoreItem::run($request);
+    }
+}
