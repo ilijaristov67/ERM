be7f121b3ee010019c239157567ae319f617f2a2
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-16 21:26:11 +0100

    added next max number helper

---
+++ app/Helpers/getNextMaxNumberHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers;
+
+class getNextMaxNumberHelper
+{
+    public function __construct(protected mixed $maxNumber)
+    {}
+
+    public function handle(): int
+    {
+        return (int) ($this->maxNumber ? $this->maxNumber + 1 : $this->maxNumber);
+    }
+}
