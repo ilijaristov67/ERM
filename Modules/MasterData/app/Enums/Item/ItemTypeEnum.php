1ad831115394e47e46696aa964683c9827abf84f
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-16 20:36:10 +0100

    added enum and migration for items

---
+++ Modules/MasterData/app/Enums/Item/ItemTypeEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\MasterData\Enums\Item;
+
+use App\Traits\EnumToArray;
+
+enum ItemTypeEnum: string
+{
+    use EnumToArray;
+    case RAW_MATERIAL = 'raw_material';
+
+    case FINISHED_MATERIAL = 'finished_material';
+
+    case TRADE_GOOD =  'trade_good';
+}
