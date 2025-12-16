a06c23dfdc473855d6764b7e5ed42bc5379e85ee
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-16 21:32:33 +0100

    added request

--- Modules/MasterData/app/Http/Requests/Item/StoreItemRequest.php
+++ Modules/MasterData/app/Http/Requests/Item/StoreItemRequest.php
@@ -3,18 +3,29 @@
 namespace Modules\MasterData\Http\Requests\Item;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Modules\MasterData\Enums\Item\ItemTypeEnum;
 
 class StoreItemRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-
+            'name' => [
+                'required',
+                'string',
+                'unique:items'
+            ],
+            'type' => [
+                'required',
+                'string',
+                Rule::in(ItemTypeEnum::values())
+            ]
         ];
     }
 
     public function authorize(): bool
     {
-        return true;
+        return auth()->user()->can('master-data-items-create');
     }
 }
---
+++ Modules/MasterData/database/seeders/Permission/PermissionSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\MasterData\Database\Seeders\Permission;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionSeeder extends Seeder
+{
+    public const string MODULE_NAME = 'master-data';
+
+    public const string GUARD_NAME = 'api';
+
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        collect(['-create', '-update', '-read', '-delete'])
+            ->crossJoin(collect([
+                '-items',
+            ]))
+            ->map(function ($crudPermission) {
+                return static::MODULE_NAME.$crudPermission[1].$crudPermission[0];
+            })->each(function (string $permission) {
+                Permission::firstOrCreate(['name' => $permission], ['guard_name' => self::GUARD_NAME]);
+            });
+    }
+}
