146b5eabe49decac83031789d64d7076d158395d
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-17 14:49:46 +0100

    added route

--- Modules/MasterData/routes/api.php
+++ Modules/MasterData/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\MasterData\Http\Controllers\Item\PatchItemController;
 use Modules\MasterData\Http\Controllers\Item\StoreItemController;
 
 Route::prefix('/items')
     ->name('items.')
     ->group(function () {
         Route::post('/', StoreItemController::class)->name('store')->middleware(['permission:master-data-items-create']);
+        Route::patch('/{item}', PatchItemController::class)->name('patch')->middleware(['permission:master-data-items-update']);
     });
