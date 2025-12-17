a5902f418e57e14d8ca38f81e756c18ced0ef311
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-17 14:32:41 +0100

    added request

---
+++ Modules/MasterData/app/Http/Requests/Item/PatchItemRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\MasterData\Http\Requests\Item;
+
+use App\Helpers\RequiredWithoutAllHelper;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Modules\MasterData\Enums\Item\ItemTypeEnum;
+
+class PatchItemRequest extends FormRequest
+{
+    private const array COLUMNS = [
+        'name',
+        'type',
+    ];
+
+    public function rules(): array
+    {
+        $requiredWithoutAll = new RequiredWithoutAllHelper(collect(self::COLUMNS));
+        return [
+            'name' => [
+                'nullable',
+                'string',
+                'unique:items',
+                'required_without_all:'.$requiredWithoutAll->handle('name'),
+                ],
+            'type' => [
+                'nullable',
+                'string',
+                Rule::in(ItemTypeEnum::values()),
+                'required_without_all:'.$requiredWithoutAll->handle('type'),
+            ]
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('master-data-items-update');
+    }
+}
