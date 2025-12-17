c08733fd33388f61b970d843adf9b833b1586836
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-17 15:10:45 +0100

    added request

---
+++ Modules/MasterData/app/Http/Requests/Item/IndexItemRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Modules\MasterData\Http\Requests\Item;
+
+use App\Http\Requests\BaseIndexRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexItemRequest extends BaseIndexRequest
+{
+    private const array  SORTABLE = [
+        'id',
+        'name',
+        'type',
+        'code'
+    ];
+
+    /** @var list<string> */
+    protected static array $sortArray = [];
+
+    public function rules(): array
+    {
+        static::$sortArray = self::SORTABLE;
+
+        return array_merge(parent::rules(), [
+            'filter.id' => [
+                'sometimes',
+                'integer',
+                'exists:items,id',
+            ],
+            'filter.name' => [
+                'sometimes',
+                'string',
+            ],
+            'filter.type' => [
+                'sometimes',
+                'string',
+            ],
+            'filter.code' => [
+                'sometimes',
+                'string',
+            ]
+        ]);
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('master-data-items-read');
+    }
+}
