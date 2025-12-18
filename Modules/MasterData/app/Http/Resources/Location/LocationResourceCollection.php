98a50870805b6741f64b2b0b4e85a385d685addc
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-18 18:25:29 +0100

    added collection

---
+++ Modules/MasterData/app/Http/Resources/Location/LocationResourceCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\MasterData\Http\Resources\Location;
+
+use App\Http\Resources\BaseJsonResource;
+use App\Traits\ResourceCollectionToArray;
+
+class LocationResourceCollection extends BaseJsonResource
+{
+    use ResourceCollectionToArray;
+}
