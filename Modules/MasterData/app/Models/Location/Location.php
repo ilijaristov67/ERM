11a66c85ccb553c3562b534b99bf3748deb50fa5
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-18 18:00:25 +0100

    added stuff

--- Modules/MasterData/app/Models/Location/Location.php
+++ Modules/MasterData/app/Models/Location/Location.php
@@ -5,6 +5,10 @@ namespace Modules\MasterData\Models\Location;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ */
+
 class Location extends Model
 {
     use SoftDeletes;
