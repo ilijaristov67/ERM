f13f1fc8a8b899566fb928c6f2ce214b29de06b1
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-18 18:57:55 +0100

    added action for store location

--- Modules/MasterData/app/Actions/Location/StoreLocation.php
+++ Modules/MasterData/app/Actions/Location/StoreLocation.php
@@ -3,10 +3,18 @@
 namespace Modules\MasterData\Actions\Location;
 
 use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\MasterData\Http\Requests\Location\StoreLocationRequest;
+use Modules\MasterData\Http\Resources\Location\LocationResource;
+use Modules\MasterData\Models\Location\Location;
 
 class StoreLocation
 {
     use AsAction;
 
-    //    public function handle():
-}
+        public function handle(StoreLocationRequest $request): LocationResource
+        {
+            $location = Location::query()->create($request->validated());
+            return LocationResource::make($location);
+        }
+    }
+
