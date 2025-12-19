d06e977f81d0190849295020613d68730b60bde1
ilijaristov67 <ilijaristov67@gmail.com>
2025-12-19 17:21:39 +0100

    added location action for patch

---
+++ Modules/MasterData/app/Actions/Location/IndexLocation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\MasterData\Actions\Location;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\MasterData\Http\Requests\Location\IndexLocationRequest;
+use Modules\MasterData\Http\Resources\Location\LocationResourceCollection;
+use Modules\MasterData\Models\Location\Location;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class IndexLocation
+{
+    use AsAction;
+
+    public function handle(IndexLocationRequest $request): LocationResourceCollection
+    {
+        $locations = QueryBuilder::for(Location::class)
+            ->allowedFilters([
+                AllowedFilter::exact('id'),
+                AllowedFilter::partial('name'),
+                AllowedFilter::partial('type'),
+                AllowedFilter::partial('capacity'),
+                AllowedFilter::partial('is_virtual'),
+            ])
+            ->allowedSorts([
+                'id',
+                'name',
+                'type',
+                'capacity',
+                'is_virtual',
+            ])
+            ->defaultSort('-id')
+            ->paginate(
+                perPage: $request->input('limit'),
+                page: $request->input('page'),
+            );
+
+        return LocationResourceCollection::make($locations);
+    }
+}
