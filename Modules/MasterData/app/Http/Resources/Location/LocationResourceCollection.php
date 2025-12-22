aacb05085663981b625428f7835319fc78e8437a
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-22 17:07:57 +0100

    pint

--- Modules/MasterData/app/Http/Resources/Location/LocationResourceCollection.php
+++ Modules/MasterData/app/Http/Resources/Location/LocationResourceCollection.php
@@ -2,7 +2,6 @@
 
 namespace Modules\MasterData\Http\Resources\Location;
 
-use App\Http\Resources\BaseJsonResource;
 use App\Http\Resources\BaseResourceCollection;
 use App\Traits\ResourceCollectionToArray;
 
