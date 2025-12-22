dba25c707972271c7495ddfa8930e33c51c0fdb3
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-22 17:24:10 +0100

    added route

--- Modules/MasterData/routes/api.php
+++ Modules/MasterData/routes/api.php
@@ -5,6 +5,7 @@ use Modules\MasterData\Http\Controllers\Item\DeleteItemController;
 use Modules\MasterData\Http\Controllers\Item\IndexItemController;
 use Modules\MasterData\Http\Controllers\Item\PatchItemController;
 use Modules\MasterData\Http\Controllers\Item\StoreItemController;
+use Modules\MasterData\Http\Controllers\Location\DeleteLocationController;
 use Modules\MasterData\Http\Controllers\Location\IndexLocationController;
 use Modules\MasterData\Http\Controllers\Location\PatchLocationController;
 use Modules\MasterData\Http\Controllers\Location\StoreLocationController;
@@ -24,4 +25,5 @@ Route::prefix('/locations')
         Route::post('/', StoreLocationController::class)->name('store')->middleware(['permission:master-data-locations-create']);
         Route::patch('/{location}', PatchLocationController::class)->name('patch')->middleware(['permission:master-data-locations-update']);
         Route::get('/', IndexLocationController::class)->name('index')->middleware(['permission:master-data-locations-read']);
+        Route::delete('/{location}', DeleteLocationController::class)->name('delete')->middleware(['permission:master-data-locations-delete']);
     });
