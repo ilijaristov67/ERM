b2851e813bcef1dcf3625affa082eff9f4e0f417
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-23 16:18:51 +0100

    added migration

---
+++ Modules/Inventory/database/migrations/2025_12_23_151735_create_inventory_quantity_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('inventory_quantity', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('location_id')->constrained()->cascadeOnDelete();
+            $table->string('quantity');
+            $table->timestamps();
+
+            $table->unique(['item_id', 'location_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_quantity');
+    }
+};
