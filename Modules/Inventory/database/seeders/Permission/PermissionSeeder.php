49a3ef46a30ad050baadcda6b7fbc200cf96178a
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-23 16:16:17 +0100

    removed unused permission

--- Modules/Inventory/database/seeders/Permission/PermissionSeeder.php
+++ Modules/Inventory/database/seeders/Permission/PermissionSeeder.php
@@ -18,7 +18,7 @@ class PermissionSeeder extends Seeder
 
         collect(['-create', '-update', '-read', '-delete'])
             ->crossJoin(collect([
-                '-products',
+                //
             ]))
             ->map(function ($crudPermission) {
                 return static::MODULE_NAME.$crudPermission[1].$crudPermission[0];
