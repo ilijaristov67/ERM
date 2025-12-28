226d7de3849254b7e692aa48d3f7e099c2f2c16d
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-28 21:33:09 +0100

    improved action

--- Modules/Procurement/app/Actions/Supplier/StoreSupplier.php
+++ Modules/Procurement/app/Actions/Supplier/StoreSupplier.php
@@ -4,10 +4,15 @@ namespace Modules\Procurement\Actions\Supplier;
 
 use Lorisleiva\Actions\Concerns\AsAction;
 use Modules\Procurement\Http\Requests\Supplier\StoreSupplierRequest;
+use Modules\Procurement\Http\Resources\Supplier\SupplierResource;
+use Modules\Procurement\Models\Supplier\Supplier;
 
 class StoreSupplier
 {
     use AsAction;
 
-    public function handle(StoreSupplierRequest $request) {}
+    public function handle(StoreSupplierRequest $request): SupplierResource
+    {
+        return SupplierResource::make(Supplier::create($request->validated()));
+    }
 }
