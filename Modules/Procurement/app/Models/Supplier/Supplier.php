ef2726ee6ae88dbe600ab678543848a2c3f6e34b
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-28 20:41:10 +0100

    added propertries

--- Modules/Procurement/app/Models/Supplier/Supplier.php
+++ Modules/Procurement/app/Models/Supplier/Supplier.php
@@ -2,10 +2,23 @@
 
 namespace Modules\Procurement\Models\Supplier;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+/**
+ * @property int $id
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $deleted_at
+ */
 class Supplier extends Model
 {
     use SoftDeletes;
+
+    protected $table = 'suppliers';
+
+    protected $fillable = [
+        'name',
+    ];
 }
