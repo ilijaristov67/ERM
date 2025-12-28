14784e9771ebbe37f2d4b70eebdf353077011c75
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-28 20:38:20 +0100

    added supplier

---
+++ Modules/Procurement/app/Models/Supplier/Supplier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Procurement\Models\Supplier;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+    use SoftDeletes;
+}
