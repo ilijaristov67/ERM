4125c3454589c1827537b17fe899ef517b4e4f4f
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-29 17:06:36 +0100

    added route

--- Modules/Procurement/routes/api.php
+++ Modules/Procurement/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Procurement\Http\Controllers\Supplier\PatchSupplierController;
 use Modules\Procurement\Http\Controllers\Supplier\StoreSupplierController;
 
 Route::prefix('/suppliers')
     ->name('suppliers.')
     ->group(function () {
         Route::post('/', StoreSupplierController::class)->name('store')->middleware(['permission:procurement-suppliers-create']);
+        Route::patch('/{supplier}', PatchSupplierController::class)->name('patch')->middleware(['permission:procurement-suppliers-update']);
     });
