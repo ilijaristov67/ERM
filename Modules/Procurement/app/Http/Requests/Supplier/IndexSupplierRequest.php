23b1f5dd3db9601c01ff0615bf431f03946eaef6
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-29 17:15:58 +0100

    added index request

---
+++ Modules/Procurement/app/Http/Requests/Supplier/IndexSupplierRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Procurement\Http\Requests\Supplier;
+
+use App\Http\Requests\BaseIndexRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexSupplierRequest extends BaseIndexRequest
+{
+    private const array SORTABLE = [
+        'id',
+        'name',
+    ];
+
+
+    /** @var list<string> */
+    protected static array $sortArray = [];
+
+    public function rules(): array
+    {
+        static::$sortArray = self::SORTABLE;
+
+        return array_merge(parent::rules(), [
+            'filter.id' => [
+                'sometimes',
+                'integer',
+                'exists:suppliers,id',
+            ],
+            'filter.name' => [
+                'sometimes',
+                'string',
+            ]
+
+        ]);
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('procurement-suppliers-read');
+    }
+}
