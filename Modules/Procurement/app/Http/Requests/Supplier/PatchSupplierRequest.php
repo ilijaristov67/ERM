51d7cb5f95033dc187fef41d621b7f4405ea2284
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-29 17:02:01 +0100

    added patch request

---
+++ Modules/Procurement/app/Http/Requests/Supplier/PatchSupplierRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Procurement\Http\Requests\Supplier;
+
+use App\Helpers\RequiredWithoutAllHelper;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatchSupplierRequest extends FormRequest
+{
+    private const array COLUMNS = [
+        'name'
+    ];
+
+    public function rules(): array
+    {
+        $supplier = $this->route('supplier');
+        $requiredWithoutAll = new RequiredWithoutAllHelper(collect(self::COLUMNS));
+
+        return [
+            'name' => [
+                'nullable',
+                'string',
+                'unique:suppliers,name,' . $supplier->id,
+                'required_without_all:' . $requiredWithoutAll->handle('name')
+            ]
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return auth()->user()->can('procurement-suppliers-update');
+    }
+}
