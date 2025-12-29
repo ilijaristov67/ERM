4f01ff94c852cbf2020cc79468ddcec19f71888e
Ilija Ristov <ilijaristov67@gmail.com>
2025-12-29 17:25:04 +0100

    added resource collection

---
+++ Modules/Procurement/app/Actions/Supplier/IndexSupplier.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Procurement\Actions\Supplier;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\Procurement\Http\Requests\Supplier\IndexSupplierRequest;
+use Modules\Procurement\Http\Resources\Supplier\SupplierResourceCollection;
+use Modules\Procurement\Models\Supplier\Supplier;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class IndexSupplier
+{
+    use AsAction;
+
+    public function handle(IndexSupplierRequest $request): SupplierResourceCollection
+    {
+        $suppliers = QueryBuilder::for(Supplier::class)
+            ->allowedFilters([
+                AllowedFilter::exact('id'),
+                AllowedFilter::partial('name'),
+            ])
+            ->allowedSorts([
+                'id',
+                'name'
+            ])
+            ->defaultSort('-id')
+            ->paginate(
+                perPage: $request->input('limit'),
+                page: $request->input('page'),
+            );
+
+
+        return SupplierResourceCollection::make($suppliers);
+    }
+}
