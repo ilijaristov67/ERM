8996c0a4f11d9a08347fbfbf14f3f92b021e1e64
ilijaristov67 <ilijaristov67@gmail.com>
2026-02-13 15:29:57 +0100

    done with action

--- Modules/Procurement/app/Actions/Supplier/DeleteSupplier.php
+++ Modules/Procurement/app/Actions/Supplier/DeleteSupplier.php
@@ -5,6 +5,7 @@ namespace Modules\Procurement\Actions\Supplier;
 use App\Http\Resources\SuccessfulOperationMessageResource;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Modules\Procurement\Models\Supplier\Supplier;
+use Symfony\Component\HttpFoundation\Response;
 
 class DeleteSupplier
 {
@@ -12,6 +13,13 @@ class DeleteSupplier
 
     public function handle(Supplier $supplier): SuccessfulOperationMessageResource
     {
+        $supplier->delete();
 
+        return SuccessfulOperationMessageResource::make([
+            'message' => __('Successfully deleted', [
+                'entity' => __('entities.item'),
+            ]),
+            'code' => Response::HTTP_OK,
+        ]);
     }
 }
