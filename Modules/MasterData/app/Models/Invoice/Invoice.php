340f17ca4b26d1b29c1cfeb7cc455f1207aaed45
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-22 23:25:09 +0100

    added factory

--- Modules/MasterData/app/Models/Invoice/Invoice.php
+++ Modules/MasterData/app/Models/Invoice/Invoice.php
@@ -3,11 +3,25 @@
 namespace Modules\MasterData\Models\Invoice;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\MasterData\Database\Factories\Invoice\InvoiceFactory;
 
 /**
  * @property int $id
  * @property Carbon $created_at
  * @property Carbon $updated_at
  */
-class Invoice extends Model {}
+class Invoice extends Model
+{
+    /** @use HasFactory<InvoiceFactory> */
+    use HasFactory;
+
+    protected $table = 'invoice';
+    protected $fillable = [];
+
+    public static function newFactory(): InvoiceFactory
+    {
+        return InvoiceFactory::new();
+    }
+}
---
+++ Modules/MasterData/database/factories/Invoice/InvoiceFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\MasterData\Database\Factories\Invoice;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\MasterData\Models\Invoice\Invoice;
+
+class InvoiceFactory extends Factory
+{
+    protected $model = Invoice::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
