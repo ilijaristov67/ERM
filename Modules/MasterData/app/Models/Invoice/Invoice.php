1fb58f225bbd8d04bd91c201a78af27613b36702
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-22 23:23:14 +0100

    generated model

---
+++ Modules/MasterData/app/Models/Invoice/Invoice.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\MasterData\Models\Invoice;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Invoice extends Model
+{
+
+}
