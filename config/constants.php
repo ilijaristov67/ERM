55d7135d0806a6a41f475394aa44bdd1adabb962
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-22 23:47:24 +0100

    added constants

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'front_end_date_format' => 'd-m-Y',
+    'front_end_date_time_format' => 'd-m-Y H:i:s',
+    'database_date_format' => 'Y-m-d',
+    'database_date_time_format' => 'Y-m-d H:i:s',
+];
