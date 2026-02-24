b07206a0d9cfc3ad1031796796033723713209bf
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-24 18:29:48 +0100

    pint

--- Modules/Import/app/Http/Requests/Import/PatchImportRequest.php
+++ Modules/Import/app/Http/Requests/Import/PatchImportRequest.php
@@ -17,25 +17,26 @@ class PatchImportRequest extends FormRequest
     public function rules(): array
     {
         $requiredWithoutAll = new RequiredWithoutAllHelper(collect(self::COLUMNS));
+
         return [
             'import_date' => [
                 'nullable',
                 'date',
                 'before_or_equal:today',
-                'required_without_all:' . $requiredWithoutAll->handle('import_date'),
+                'required_without_all:'.$requiredWithoutAll->handle('import_date'),
             ],
             'supplier_id' => [
                 'nullable',
                 'integer',
                 'exists:suppliers,id',
-                'required_without_all:' . $requiredWithoutAll->handle('supplier_id'),
+                'required_without_all:'.$requiredWithoutAll->handle('supplier_id'),
             ],
             'invoice_id' => [
                 'nullable',
                 'integer',
                 'exists:invoices,id',
-                'required_without_all:' . $requiredWithoutAll->handle('invoice_id'),
-            ]
+                'required_without_all:'.$requiredWithoutAll->handle('invoice_id'),
+            ],
         ];
     }
 
