e8c7b77c7981743decb4868dbe6f4d5d8d46d1fe
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-24 16:01:24 +0100

    added import store action

---
+++ Modules/Import/app/Actions/Import/StoreImport.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Import\Actions\Import;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\Import\Http\Resources\Import\ImportResource;
+
+class StoreImport
+{
+    use AsAction;
+
+    public function handle(): ImportResource
+    {
+        //
+    }
+}
