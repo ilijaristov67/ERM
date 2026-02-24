d66be081594b223bb2cd4235f14492411ee672c0
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-24 16:33:26 +0100

    added action for import

--- Modules/Import/app/Actions/Import/StoreImport.php
+++ Modules/Import/app/Actions/Import/StoreImport.php
@@ -3,14 +3,18 @@
 namespace Modules\Import\Actions\Import;
 
 use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\Import\Http\Import;
+use Modules\Import\Http\Requests\Import\StoreImportRequest;
 use Modules\Import\Http\Resources\Import\ImportResource;
 
 class StoreImport
 {
     use AsAction;
 
-    public function handle(): ImportResource
+    public function handle(StoreImportRequest $request): ImportResource
     {
-        //
+        $import = Import::create($request->validated());
+
+        return new ImportResource($import->loadMissing('supplier', 'user', 'invoice'));
     }
 }
