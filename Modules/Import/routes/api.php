6ab3e0cd7879cd41c483888effbf20ef222002de
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-24 22:53:28 +0100

    added route

--- Modules/Import/routes/api.php
+++ Modules/Import/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Import\Http\Controllers\Import\IndexImportController;
 use Modules\Import\Http\Controllers\Import\PatchImportController;
 use Modules\Import\Http\Controllers\Import\StoreImportController;
 
@@ -9,4 +10,5 @@ Route::prefix('/')
     ->group(function () {
         Route::post('', StoreImportController::class)->name('store')->middleware('permission:import-create');
         Route::patch('/{import}', PatchImportController::class)->name('patch')->middleware('permission:import-update');
+        Route::get('/', IndexImportController::class)->name('index')->middleware('permission:import-read');
     });
