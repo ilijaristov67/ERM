de2e7909238a0dc1c05899a9eeea05cc0cc49b5a
Ilija Ristov <ilijaristov67@gmail.com>
2026-02-24 18:45:48 +0100

    added controller

---
+++ Modules/Import/app/Http/Controllers/Import/PatchImportController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Import\Http\Controllers\Import;
+
+use App\Http\Controllers\Controller;
+use Modules\Import\Actions\Import\PatchImport;
+use Modules\Import\Http\Import;
+use Modules\Import\Http\Requests\Import\PatchImportRequest;
+use Modules\Import\Http\Resources\Import\ImportResource;
+
+class PatchImportController extends Controller
+{
+    public function __invoke(PatchImportRequest $request, Import $import): ImportResource
+    {
+        return PatchImport::run($import, $request);
+    }
+}
