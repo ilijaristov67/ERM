f1884e59b59b18462080c700c6ad71cd1962b305
Ilija Ristov <ilijaristov67@gmail.com>
2026-03-03 14:26:39 +0100

    filed for resource

---
+++ Modules/Import/app/Http/Resources/Import/Lot/ImportLotResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Import\Http\Resources\Import\Lot;
+
+use App\Http\Resources\BaseJsonResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ImportLotResource extends BaseJsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+
+        ];
+    }
+}
