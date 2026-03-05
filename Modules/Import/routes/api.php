1f044704f2b9b1bd236ae9536ab9aac4c1dd92c2
Ilija Ristov <ilijaristov67@gmail.com>
2026-03-05 19:15:38 +0100

    added route

--- Modules/Import/routes/api.php
+++ Modules/Import/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Import\Http\Controllers\Import\DeleteImportController;
 use Modules\Import\Http\Controllers\Import\IndexImportController;
+use Modules\Import\Http\Controllers\Import\Lot\IndexImportLotController;
 use Modules\Import\Http\Controllers\Import\Lot\PatchImportLotController;
 use Modules\Import\Http\Controllers\Import\Lot\StoreImportLotController;
 use Modules\Import\Http\Controllers\Import\PatchImportController;
@@ -21,5 +22,6 @@ Route::prefix('/')
             ->group(function () {
                 Route::post('/', StoreImportLotController::class)->name('store')->middleware('permission:import-lots-create');
                 Route::patch('/{import_lot}', PatchImportLotController::class)->name('patch')->middleware('permission:import-lots-update');
+                Route::get('/', IndexImportLotController::class)->name('index')->middleware('permission:import-read');
             });
     });
