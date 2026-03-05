2f74782004970ace170052f32947fb7d336398e7
Ilija Ristov <ilijaristov67@gmail.com>
2026-03-05 15:12:53 +0100

    added action for store import lot

---
+++ Modules/Import/app/Actions/Import/Lot/StoreImportLot.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Import\Actions\Import\Lot;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+use Modules\Import\Http\Requests\Import\Lot\StoreImportLotRequest;
+use Modules\Import\Http\Resources\Import\Lot\ImportLotResource;
+use Modules\Import\Models\Import\Lot\ImportLot;
+
+class StoreImportLot
+{
+
+    use AsAction;
+
+    public function handle(StoreImportLotRequest $request): ImportLotResource
+    {
+        $importLot = ImportLot::create($request->validated());
+
+        return ImportLotResource::make($importLot);
+    }
+}
