f92afc103b0e23d780e53d1243beea538801fd53
Ilija Ristov <ilijaristov67@gmail.com>
2026-03-05 20:56:03 +0100

    finished with factory

--- Modules/Import/database/factories/Import/Lot/Item/ImportLotItemFactory.php
+++ Modules/Import/database/factories/Import/Lot/Item/ImportLotItemFactory.php
@@ -3,13 +3,20 @@
 namespace Modules\Import\Database\Factories\Import\Lot\Item;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Import\Models\Import\Lot\ImportLot;
+use Modules\Import\Models\Import\Lot\Item\ImportLotItem;
+use Modules\MasterData\Models\Item\Item;
 
 class ImportLotItemFactory extends Factory
 {
+    protected $model = ImportLotItem::class;
+
     public function definition(): array
     {
         return [
-
+        'import_lot_id' => ImportLot::factory(),
+            'item_id' => Item::factory(),
+            'quantity' => (string) $this->faker->numberBetween(1, 100),
         ];
     }
 }
